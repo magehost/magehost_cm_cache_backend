69a24adf9862bd5c4aeafc802a314329c4154b52
User 'vagrant' on 'magehostdev.pro <vagrant@magehostdev.pro>
2016-02-10 23:08:01 +0100

    Revert "Updated submodule"
    This reverts commit 9a4eed0b30e5c45344983dd730d80f95d5d61da4.

--- File.php
+++ File.php
@@ -24,7 +24,8 @@ class MageHost_Cm_Cache_Backend_File extends Cm_Cache_Backend_File
     protected $frontendPrefix = null;
 
     /**
-     * This method will dispatch the event 'magehost_clean_backend_cache'.
+     * This method will dispatch the events 'magehost_clean_backend_cache_before'
+     *                                  and 'magehost_clean_backend_cache_after'.
      * Event listeners can change the mode or tags.
      *
      * {@inheritdoc}
@@ -36,12 +37,16 @@ class MageHost_Cm_Cache_Backend_File extends Cm_Cache_Backend_File
         /** @noinspection PhpUndefinedMethodInspection */
         $transportObject->setTags( $tags );
         Mage::dispatchEvent( 'jv_clean_backend_cache', array( 'transport' => $transportObject ) ); // deprecated
-        Mage::dispatchEvent( 'magehost_clean_backend_cache', array( 'transport' => $transportObject ) );
+        Mage::dispatchEvent( 'magehost_clean_backend_cache_before', array( 'transport' => $transportObject ) );
         /** @noinspection PhpUndefinedMethodInspection */
         $mode = $transportObject->getMode();
         /** @noinspection PhpUndefinedMethodInspection */
         $tags = $transportObject->getTags();
-        return parent::clean($mode, $tags);
+        $result = parent::clean($mode, $tags);
+        $transportObject->setResult( $result );
+        Mage::dispatchEvent( 'magehost_clean_backend_cache_after', array( 'transport' => $transportObject ) );
+        $result = $transportObject->getResult();
+        return $result;
     }
 
     /**
@@ -53,8 +58,8 @@ class MageHost_Cm_Cache_Backend_File extends Cm_Cache_Backend_File
     public function load($id, $doNotTestCacheValidity = false) {
         $result = parent::load($id, $doNotTestCacheValidity);
         if ( false === $result && false !== strpos($id,'_JV_') ) {
-            Mage::dispatchEvent('jv_cache_miss_jv', array('id' => $id));       // deprecated
-            Mage::dispatchEvent('magehost_cache_miss_jv', array('id' => $id)); // deprecated
+            Mage::dispatchEvent('jv_cache_miss_jv', array('id' => $id)); // deprecated
+            Mage::dispatchEvent('magehost_cache_miss_jv', array('id' => $id));
         }
         if ( false === $result && false !== strpos($id,'_MH_') ) {
             Mage::dispatchEvent('magehost_cache_miss_mh', array('id' => $id));
