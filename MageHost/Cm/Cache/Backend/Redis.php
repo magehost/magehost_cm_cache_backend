bab1cb93cda4929ac32646a55f077dfaad598da3
Jeroen Vermeulen - MageHost <jeroen@magehost.pro>
2018-05-17 16:13:05 +0200

    v1.0.1: Working in Magento 2 + Stripped functionality not used

--- MageHost/Cm/Cache/Backend/Redis.php
+++ MageHost/Cm/Cache/Backend/Redis.php
@@ -14,13 +14,15 @@
 /**
  * {@inheritdoc}
  */
-class MageHost_Cm_Cache_Backend_Redis extends Cm_Cache_Backend_Redis
+class MageHost_Cm_Cache_Backend_Redis extends \Cm_Cache_Backend_Redis
 {
     const ADMIN_READ_TIMEOUT = 7200;
     /** @var bool - Only true when constructor was successful. */
     protected $works = false;
     /** @var string|null */
     protected $frontendPrefix = null;
+    /** @var \Magento\Framework\Event\ManagerInterface */
+    protected $eventManager;
 
     /**
      * This constructor is executed in a very early stage, during @see Mage_Core_Model_App->initCache()
@@ -29,25 +31,23 @@ class MageHost_Cm_Cache_Backend_Redis extends Cm_Cache_Backend_Redis
      * {@inheritdoc}
      */
     public function __construct( $options ) {
-        if ( $this->isAdmin() ) {
-            if ( empty($options['read_timeout']) || $options['read_timeout'] < self::ADMIN_READ_TIMEOUT ) {
-                $options['read_timeout'] = self::ADMIN_READ_TIMEOUT;
-            }
-        }
         try {
             parent::__construct( $options );
             $this->works = true;
-        } catch ( CredisException $e ) {
+        } catch ( \CredisException $e ) {
             $this->processRedisException( $e, 'constructor' );
-        } catch ( RedisException $e ) {
+        } catch ( \RedisException $e ) {
             $this->processRedisException( $e, 'constructor' );
-        } catch ( Zend_Cache_Exception $e ) {
+        } catch ( \Zend_Cache_Exception $e ) {
             $this->processRedisException( $e, 'constructor' );
         }
         if ( !$this->works ) {
             // We can't use Zend_Log here because it may not be loaded yet.
             error_log( sprintf( '%s: Disabled Redis cache backend because constructor failed', __CLASS__ ) );
         }
+        // There is no other way since the Cache Backend is a classic Zend class.
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        $this->eventManager = $objectManager->create('\Magento\Framework\Event\Manager');
     }
 
     /**
@@ -64,23 +64,16 @@ class MageHost_Cm_Cache_Backend_Redis extends Cm_Cache_Backend_Redis
         if ( $this->works ) {
             try {
                 $result = parent::load( $id, $doNotTestCacheValidity );
-            } catch ( CredisException $e ) {
+            } catch ( \CredisException $e ) {
                 $this->processRedisException( $e, 'load' );
                 $result = false;
-            } catch ( RedisException $e ) {
+            } catch ( \RedisException $e ) {
                 $this->processRedisException( $e, 'load' );
                 $result = false;
-            } catch ( Zend_Cache_Exception $e ) {
+            } catch ( \Zend_Cache_Exception $e ) {
                 $this->processRedisException( $e, 'load' );
                 $result = false;
             }
-        }
-        if ( false === $result && false !== strpos($id,'_JV_') ) {
-            Mage::dispatchEvent('jv_cache_miss_jv', array('id' => $id)); // deprecated
-            Mage::dispatchEvent('magehost_cache_miss_jv', array('id' => $id));
-        }
-        if ( false === $result && false !== strpos($id,'_MH_') ) {
-            Mage::dispatchEvent('magehost_cache_miss_mh', array('id' => $id));
         }
         return $result;
     }
@@ -96,13 +89,13 @@ class MageHost_Cm_Cache_Backend_Redis extends Cm_Cache_Backend_Redis
         if ( $this->works ) {
             try {
                 $result = parent::test( $id );
-            } catch ( CredisException $e ) {
+            } catch ( \CredisException $e ) {
                 $this->processRedisException( $e, 'test' );
                 $result = false;
-            } catch ( RedisException $e ) {
+            } catch ( \RedisException $e ) {
                 $this->processRedisException( $e, 'test' );
                 $result = false;
-            } catch ( Zend_Cache_Exception $e ) {
+            } catch ( \Zend_Cache_Exception $e ) {
                 $this->processRedisException( $e, 'test' );
                 $result = false;
             }
@@ -121,25 +114,16 @@ class MageHost_Cm_Cache_Backend_Redis extends Cm_Cache_Backend_Redis
     public function save($data, $id, $tags = array(), $specificLifetime = false)
     {
         $result = false;
-        if ( in_array( $this->getFrontendPrefix().'BLOCK_HTML', $tags ) ) {
-            $transportObject = new Varien_Object;
-            /** @noinspection PhpUndefinedMethodInspection */
-            $transportObject->setTags($tags);
-            Mage::dispatchEvent('jv_cache_save_block', array('id' => $id,'transport' => $transportObject)); // deprecated
-            Mage::dispatchEvent('magehost_cache_save_block', array('id' => $id,'transport' => $transportObject));
-            /** @noinspection PhpUndefinedMethodInspection */
-            $tags = $transportObject->getTags();
-        }
         if ( $this->works ) {
             try {
                 $result = parent::save( $data, $id, $tags, $specificLifetime );
-            } catch ( CredisException $e ) {
+            } catch ( \CredisException $e ) {
                 $this->processRedisException( $e, 'save' );
                 $result = false;
-            } catch ( RedisException $e ) {
+            } catch ( \RedisException $e ) {
                 $this->processRedisException( $e, 'save' );
                 $result = false;
-            } catch ( Zend_Cache_Exception $e ) {
+            } catch ( \Zend_Cache_Exception $e ) {
                 $this->processRedisException( $e, 'save' );
                 $result = false;
             }
@@ -158,13 +142,13 @@ class MageHost_Cm_Cache_Backend_Redis extends Cm_Cache_Backend_Redis
         if ( $this->works ) {
             try {
                 $result = parent::remove( $id );
-            } catch ( CredisException $e ) {
+            } catch ( \CredisException $e ) {
                 $this->processRedisException( $e, 'remove' );
                 $result = false;
-            } catch ( RedisException $e ) {
+            } catch ( \RedisException $e ) {
                 $this->processRedisException( $e, 'remove' );
                 $result = false;
-            } catch ( Zend_Cache_Exception $e ) {
+            } catch ( \Zend_Cache_Exception $e ) {
                 $this->processRedisException( $e, 'remove' );
                 $result = false;
             }
@@ -180,16 +164,15 @@ class MageHost_Cm_Cache_Backend_Redis extends Cm_Cache_Backend_Redis
      *
      * {@inheritdoc}
      */
-    public function clean($mode = Zend_Cache::CLEANING_MODE_ALL, $tags = array())
+    public function clean($mode = \Zend_Cache::CLEANING_MODE_ALL, $tags = array())
     {
         $result = false;
-        $transportObject = new Varien_Object;
+        $transportObject = new \Varien_Object;
         /** @noinspection PhpUndefinedMethodInspection */
         $transportObject->setMode( $mode );
         /** @noinspection PhpUndefinedMethodInspection */
         $transportObject->setTags( $tags );
-        Mage::dispatchEvent( 'jv_clean_backend_cache', array( 'transport' => $transportObject ) ); // deprecated
-        Mage::dispatchEvent( 'magehost_clean_backend_cache_before', array( 'transport' => $transportObject ) );
+        $this->eventManager->dispatch( 'magehost_clean_backend_cache_before', array( 'transport' => $transportObject ) );
         /** @noinspection PhpUndefinedMethodInspection */
         $mode = $transportObject->getMode();
         /** @noinspection PhpUndefinedMethodInspection */
@@ -197,19 +180,19 @@ class MageHost_Cm_Cache_Backend_Redis extends Cm_Cache_Backend_Redis
         if ( $this->works ) {
             try {
                 $result = parent::clean($mode, $tags);
-            } catch ( CredisException $e ) {
+            } catch ( \CredisException $e ) {
                 $this->processRedisException( $e, 'remove' );
                 $result = false;
-            } catch ( RedisException $e ) {
+            } catch ( \RedisException $e ) {
                 $this->processRedisException( $e, 'remove' );
                 $result = false;
-            } catch ( Zend_Cache_Exception $e ) {
+            } catch ( \Zend_Cache_Exception $e ) {
                 $this->processRedisException( $e, 'remove' );
                 $result = false;
             }
         }
         $transportObject->setResult( $result );
-        Mage::dispatchEvent( 'magehost_clean_backend_cache_after', array( 'transport' => $transportObject ) );
+        $this->eventManager->dispatch( 'magehost_clean_backend_cache_after', array( 'transport' => $transportObject ) );
         $result = $transportObject->getResult();
         return $result;
     }
@@ -345,50 +328,5 @@ class MageHost_Cm_Cache_Backend_Redis extends Cm_Cache_Backend_Redis
                             (string)$e );
         // We can't use Zend_Log here because it may not be loaded yet.
         error_log( $message );
-    }
-
-    protected function getFrontendPrefix() {
-        if ( is_null($this->frontendPrefix) ) {
-            $this->frontendPrefix = Mage::app()->getCacheInstance()->getFrontend()->getOption('cache_id_prefix');
-        }
-        return $this->frontendPrefix;
-    }
-
-    /**
-     * Function to determine if we are currently in admin or cli.
-     * This function must work in a very early stage so we can't use Mage::app()
-     * @return bool
-     */
-    public static function isAdmin() {
-        static $result = null;
-        if ( is_null($result) ) {
-            $result = false;
-            $baseScript = basename($_SERVER['SCRIPT_FILENAME']);
-            if ( 0 === strpos($baseScript,'n98') ||
-                 0 === strpos($baseScript,'cron') ||
-                 0 === strpos($baseScript,'asyncindex') ) {
-                // CLI or Cron
-                $result = true;
-            } else {
-                $adminPath = null;
-                $config = Mage::getConfig();
-                if ( !empty($config) ) {
-                    // Not using Mage_Adminhtml_Helper_Data::XML_PATH_... here because it does not exist in older Magento versions
-                    $useCustomAdminPath = (bool)(string)$config->getNode('default/admin/url/use_custom_path');
-                    if ($useCustomAdminPath) {
-                        $adminPath = (string)$config->getNode('default/admin/url/custom_path');
-                    }
-                    if ( empty($adminPath) ) {
-                        $adminPath = (string)$config->getNode('admin/routers/adminhtml/args/frontName');
-                    }
-                    $request = new Zend_Controller_Request_Http;
-                    $pathParts = explode( '/', trim($request->getPathInfo(),'/') );
-                    if ( isset($pathParts[0]) && $pathParts[0] == $adminPath ) {
-                        $result = true;
-                    }
-                }
-            }
-        }
-        return $result;
     }
 }
--- README.md
+++ README.md
@@ -1,14 +1,10 @@
 # MageHost_Cm_Cache_Backend
 MageHost extended version of Colin Mollenhour's Cm_Cache_Backend_File and Cm_Cache_Backend_Redis
 
-## Installation
-* Install [Modman](https://github.com/colinmollenhour/modman)
-* `cd` to your Magento root dir
-* `test -d .modman || modman init`
-* `modman clone --copy --force https://github.com/magehost/magehost_cm_cache_backend`
-* `modman clone --copy --force https://github.com/colinmollenhour/Cm_Cache_Backend_File.git`
-* `modman clone --copy --force https://github.com/colinmollenhour/Cm_Cache_Backend_Redis.git`
-* If you keep your Magento code in Git: 
-  * `rm -rf lib/Credis/.git`
-  * Add `.modman` to your `.gitignore`
-* Edit `app/etc/local.xml`: inside `<config><global>` add/update:<br /> `<cache><backend>MageHost_Cm_Cache_Backend_File</backend></cache>` or `MageHost_Cm_Cache_Backend_Redis`
+## Installation using Composer
+* `cd` to your Magento 2 root dir
+```
+composer config repositories.magehost_cm_cache_backend vcs git@github.com:magehost/magehost_cm_cache_backend.git
+composer require magehost/magehost_cm_cache_backend
+```
+* Edit `app/etc/env.php`: set *cache > frontend > default > backend* to `MageHost_Cm_Cache_Backend_Redis`
